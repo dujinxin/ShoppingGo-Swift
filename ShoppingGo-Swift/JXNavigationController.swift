4741efb9c93566d01cdad56f98e4b50f0fc6a289
dujinxin <dujinxin8866@163.com>
2017-06-06 14:15:51 +0800

    添加项目

---
+++ ShoppingGo-Swift/JXNavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  JXNavigationController.swift
+//  ShoppingGo-Swift
+//
+//  Created by 杜进新 on 2017/6/6.
+//  Copyright © 2017年 杜进新. All rights reserved.
+//
+
+import UIKit
+
+class JXNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
