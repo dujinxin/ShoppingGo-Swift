019eb353643e76d9d2149106145372a288e1e386
dujinxin <dujinxin8866@163.com>
2017-06-13 09:52:28 +0800

    封装afnetworking

--- ShoppingGo/Main/MyViewController.swift
+++ ShoppingGo/Main/MyViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AFNetworking
 
 private let cellId = "cellId"
 
@@ -29,9 +30,109 @@ class MyViewController: JXTableViewController {
     
         //dataArray = NSMutableArray.init()
         dataArray.addObjects(from: ["12345","34567","234567","346578","678675645"])
+        
+        
+        
+        
+        
+        
+        let longitude = 0
+        let latitude = 0
+        var parameters = ""
+        let Channel = "appStore"
+        let Mac = ""
+        let IP = ""
+        let City = "北京市"
+        let token = ""
+        
+        parameters = "Version=2.0.1&Package=GjieGo&Channel=\(Channel)&Longitude=\(longitude)&Latitude=\(latitude)&Mac=\(Mac)&IP=\(IP)&City=\(City)"
+        
+        let ssss = "https://appc.guangjiego.com/v1/UserLogin/GetTokenByKey?\(parameters)"
+        let sssss = ssss.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        
+        
+        let success = {(task:URLSessionDataTask, jsonData:Any?)->() in
+            
+        }
+        let failure = {(task:URLSessionDataTask, error:Error)->() in
+            
+        }
+        
+        
+        
+        let manager = AFHTTPSessionManager()
+        manager.responseSerializer = AFHTTPResponseSerializer.init()
+        //manager.responseSerializer = AFHTTPResponseSerializer.j
+        manager.responseSerializer.acceptableContentTypes = NSSet(objects: "text/html") as? Set<String>
+        
+//        manager.post(sssss!, parameters: ["Uc":UIDevice.current.identifierForVendor?.uuidString], progress: nil, success: { (task:URLSessionDataTask, responseObject:Any?) in
+//            print(responseObject ?? "空")
+//            print(task)
+//            //responseObject.
+//            
+//            let isJson = JSONSerialization.isValidJSONObject(responseObject)
+//            print(isJson)
+//            
+//            guard let data = responseObject as? Data,
+//                  let jsonData = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers)
+//            
+//                else{
+//                return
+//            }
+//
+//            print(jsonData)
+//
+//        }) { (task:URLSessionDataTask?, error:Error) in
+//            print(error)
+//        }
+        
+        JXBaseRequest.request(url: "/UserLogin/GetTokenByKey", param: ["Uc":(UIDevice.current.identifierForVendor?.uuidString)!], success: { (responseObject:Any?, message:String?, alertType:String?) in
+            //
+            let isJson = JSONSerialization.isValidJSONObject(responseObject)
+            print(isJson)
+            //let jsons = JSONSerialization.jsonObject(with: <#T##InputStream#>, options: <#T##JSONSerialization.ReadingOptions#>)
+            
+            guard let data = responseObject as? Data,
+                let jsonData = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers)
+                
+                else{
+                    return
+            }
+            
+            print("jsonData = \(jsonData) \n message = \(message) \n alertType = \(alertType)")
+        }) { (task:URLSessionDataTask, error:Error?) in
+            //
+            print(error)
+        }
+        
+        
+//        var req = JXRequest(url: sssss!, param: ["Uc":(UIDevice.current.identifierForVendor?.uuidString)!]) { (task:URLSessionDataTask, responseObject:Any?, error:Error?) in
+//            print(responseObject ?? "空")
+//            print(task)
+//            //responseObject.
+//            
+//            let isJson = JSONSerialization.isValidJSONObject(responseObject)
+//            print(isJson)
+//            
+//            guard let data = responseObject as? Data,
+//                let jsonData = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers)
+//                
+//                else{
+//                    return
+//            }
+//            
+//            print(jsonData)
+//        }
+        
+        
+        
+        
+        //https://appc.guangjiego.com/v1/UserLogin/GetTokenByKey?Version=2.0.1&Package=GjieGo&Channel=appStore&Longitude=0&Latitude=0&Mac=&IP=&City=%E5%8C%97%E4%BA%AC%E5%B8%82
+        //https://appc.guangjiego.com/v1/UserLogin/GetTokenByKey?Version=2.0.1&Package=GjieGo&Channel=appStore&Longitude=0&Latitude=0&Mac=&IP=&City=%E5%8C%97%E4%BA%AC,
     }
     
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
